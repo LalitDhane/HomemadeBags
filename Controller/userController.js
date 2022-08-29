d3eb0a82460265937e18e3a92dd9b8f1949d96aa
Lalit Dhane <lalitdhane4455@gmail.com>
2022-08-29 14:08:21 +0000

    update functions with async await of userController

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,26 +1,35 @@
 //User Model
 const userModel = require("../Models/userModel");
 
-const getUser = (req, res) => {
-  userModel.find({}, (err, userData) => {
-    res.send(err || userData);
-  });
+const getUser = async (req, res) => {
+  try {
+    let userData = await userModel.find({});
+    res.send(userData);
+  } catch (error) {
+    res.send(error); 
+  }
 };
 
-const addUser = (req, res) => {
+const addUser = async (req, res) => {
   const user = new userModel({
     username: req.body.username,
     password: req.body.password,
   });
-  user.save((err, result) => {
-    res.send(err || result);
-  });
+  try {
+    let result = await user.save();
+    res.send(result); 
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const deleteUser = (req, res) => {
-  userModel.findByIdAndDelete(req.params.id, (err) => {
-    res.send(err || `${req.params.id} deleted successfully.`);
-  });
+const deleteUser = async (req, res) => {
+  try {
+    await userModel.findByIdAndDelete(req.params.id);
+    res.send(`${req.params.id} deleted successfully.`); 
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 module.exports = { getUser, addUser, deleteUser };
