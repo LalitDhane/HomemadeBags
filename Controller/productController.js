860bdee99daede67ff9d87eae82f2efa77f79c50
LalitDhane <lalitdhane4455@gmail.com>
2022-08-31 19:31:48 +0530

    updated productController with async await

--- Controller/productController.js
+++ Controller/productController.js
@@ -3,30 +3,45 @@ const ApiUtil = require("../Utils/apiUtil");
 //Models
 const productModel = require("../Models/productModel.js");
 
-const getProduct = (req, res) => {
-  productModel.find({}, (err, productData) => {
-    res.send(err || productData);
-  });
+const getProduct = async (req, res) => {
+  try {
+    let user = await productModel.find({});
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const addNewProduct = (req, res) => {
-  const newProduct = new productModel(ApiUtil.getProductObject(req));
-  newProduct.save((err) => res.json(err || "Product Inserted Successfully"));
+const addNewProduct = async (req, res) => {
+  try {
+    const newProduct = new productModel(ApiUtil.getProductObject(req));
+    await newProduct.save();
+    res.send("Product Inserted Successfully");
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const updateProduct = (req, res) => {
-  productModel.findOneAndUpdate(
-    { code: req.params.productCode },
-    { ...ApiUtil.getProductObject(req) },
-    { new: true },
-    (err) => res.send(err || `${req.params.productCode} updated Successfully`)
-  );
+const updateProduct = async (req, res) => {
+  try {
+    await productModel.findOneAndUpdate(
+      { code: req.params.productCode },
+      { ...ApiUtil.getProductObject(req) },
+      { new: true }
+    );
+    res.send(`${req.params.productCode} updated Successfully`);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const deleteProduct = (req, res) => {
-  productModel.deleteOne({ _id: req.params.productId }, (err) =>
-    res.send(err || `${req.params.productId} deleted Successfully`)
-  );
+const deleteProduct = async (req, res) => {
+  try {
+    await productModel.deleteOne({ _id: req.params.productId });
+    res.send(`${req.params.productId} deleted Successfully`);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 module.exports = { getProduct, addNewProduct, updateProduct, deleteProduct };
