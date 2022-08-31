aa575cf4bd1882b7b3ef26fbd310c5b33be199ad
LalitDhane <lalitdhane4455@gmail.com>
2022-08-31 19:51:39 +0530

    added morgan (logging)

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ require("./databaseConnection");
 const express = require("express");
 const cors = require("cors");
 const router = require("./Routes/router");
+const morgan = require("morgan");
 const app = express();
 const port = process.env.port || 3001;
 
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 //Routes
 app.use("/", router);
